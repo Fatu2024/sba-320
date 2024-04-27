9cd4cd373b9cb4e48a7e4cc308b8b251ee30170c
Fatu2024 <fatumab3@gmail.com>
2024-04-27 15:22:43 -0400

    finally got my fetch to work

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sba-320
 
+this site allows you to search a marvel character and get a list of all the comics/adaptations they've been in
+
 
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,13 +1,7 @@
 function Home () {
-    const background = {
-        backgroundImage: 'url("https://wallpapercave.com/wp/wp5786926.jpg")',
-        backgroundSize: 'cover',
-        minHeight: '100vh'
-    };
-
     return (
-        <div style={background}>
-            <div>Homepage: welcome!</div>
+        <div className='homepage'>
+            <img src="/marvel.jpg"/>
         </div>
     );
 }
